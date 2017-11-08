e6ae8ed4b68f4ec1b92ff26de4298b577d567943
Luis Villarreal <luisdaniel_1996@hotmail.com>
2017-11-07 22:15:49 -0600

    Added complete database (fixed).

---
+++ Base de datos/hospitalito_hiredservices.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hospitalito
+-- ------------------------------------------------------
+-- Server version	5.7.20-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hiredservices`
+--
+
+DROP TABLE IF EXISTS `hiredservices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hiredservices` (
+  `ServiceID` int(11) NOT NULL,
+  `ResidentID` int(11) NOT NULL,
+  `ClientNumber` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`ServiceID`,`ResidentID`),
+  KEY `ResidentID` (`ResidentID`),
+  CONSTRAINT `hiredservices_ibfk_1` FOREIGN KEY (`ServiceID`) REFERENCES `emergencyservices` (`ServiceID`),
+  CONSTRAINT `hiredservices_ibfk_2` FOREIGN KEY (`ResidentID`) REFERENCES `residents` (`ResidentID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hiredservices`
+--
+
+LOCK TABLES `hiredservices` WRITE;
+/*!40000 ALTER TABLE `hiredservices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hiredservices` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-11-07 21:37:05
